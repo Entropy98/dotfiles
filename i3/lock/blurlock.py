86e7068b43549021ac5c359a93c18d303f6f6d00
Entropy98 <harperweigle@gmail.com>
2023-04-17 16:30:11 -0700

    blurlock, spotify, fixed scroll, and vpn

---
+++ i3/lock/blurlock.py
@@ -0,0 +1,8 @@
+import pyautogui
+import os
+from PIL import Image, ImageFilter
+
+im0 = pyautogui.screenshot()
+im1 = im0.filter(ImageFilter.GaussianBlur(10))
+im1.save('/tmp/blurlock.png')
+os.popen('i3lock -i /tmp/blurlock.png')
