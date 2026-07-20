68110274bf21b7198eb02bc7bb23bd3b36320fc9
Harper Weigle <harper.weigle@echo.bio>
2026-07-20 09:13:18 -0700

    removing unused files for rosey

--- screens/3440x1440_25Hz.sh
+++
@@ -1,8 +0,0 @@
-if [[ "$(hwinfo --monitor)" == *"GN34CW"* ]]; then
-  if [[ "$(xrandr)" != *"3440x1440_25.00"* ]]; then
-    xrandr --newmode "3440x1440_25.00"  160.25  3440 3568 3912 4384  1440 1443 1453 1464 -hsync +vsync
-    xrandr --addmode HDMI-2 "3440x1440_25.00"
-  fi
-  xrandr --output HDMI-2 --mode "3440x1440_25.00"
-  xrandr --output eDP-1 --off 
-fi
